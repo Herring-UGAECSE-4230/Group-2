d03e6518d997d19a14caf4dbf3e87d97d4bd9136
Tyler Cook <twc35917@uga.edu>
2024-01-31 15:30:20 -0500

    keypad code test

--- Python/Seven Segment Display Clock and Timer/Keypad.py
+++ Python/Seven Segment Display Clock and Timer/Keypad.py
@@ -6,10 +6,10 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 
-GPIO.setup(18, GPIO.IN) #Yellow Wire X1
-GPIO.setup(23, GPIO.IN) #Orange Wire X2
-GPIO.setup(24, GPIO.IN) #Brown Wire X3
-GPIO.setup(25, GPIO.IN) #Red Wire X4
+GPIO.setup(18, GPIO.OUT) #Yellow Wire X1
+GPIO.setup(23, GPIO.OUT) #Orange Wire X2
+GPIO.setup(24, GPIO.OUT) #Brown Wire X3
+GPIO.setup(25, GPIO.OUT) #Red Wire X4
 GPIO.setup(12, GPIO.IN) #Black Wire Y1
 GPIO.setup(16, GPIO.IN) #White Wire Y2
 GPIO.setup(20, GPIO.IN) #Gray Wire Y3
@@ -19,5 +19,22 @@ GPIO.setup(21, GPIO.IN) #Blue Wire Y4
 #Defining the keymap for the keypad
 
 
+#conditional implementation of keypad I thought it would be easiest this way
+def readKeypad(rowNum, char):
+    GPIO.output(rowNum, GPIO.HIGH)
+    if GPIO.input(12) == 1:
+        curVal = char[0]
+    if GPIO.input(16) == 1:
+        curVal = char[1]
+    if GPIO.input(20) == 1:
+        curVal = char[2]
+    if GPIO.input(21) ==1:
+        curVal = char[3]
+    GPIO.output(rowNum, GPIO.LOW)
+    return curVal
 
-def readKeypad(rowNum,char):
+while True:
+    readKeypad(18,[1,2,3,'A'])
+    readKeypad(23,[1,2,3,'A'])
+    readKeypad(24,[1,2,3,'A'])
+    readKeypad(25,[1,2,3,'A'])

264d3e71df35d2c3b62f3c7cfcc09f388cea29e7
Evan Culbertson <esc93338@uga.edu>
2024-02-28 21:51:04 -0500

    ASFKNEKFAKN

--- Python/Seven Segment Display Clock and Timer/NewClock.py
+++ Python/Seven Segment Display Clock and Timer/NewClock.py
@@ -45,10 +45,8 @@ enable = True
 global position
 position = 1
 last = [0,0,0,0]
-escape = 0
 global now
 now = time.time()
-
 
 # Maps the segments on the SSD to the corresponding keypad values
 seven_segment_map = {
@@ -118,7 +116,6 @@ def read_keypad():
                     return key_lut[row * 4 + col]
         GPIO.output(rowPins, GPIO.LOW)
         
-
 def loadDisplay(Clk, value):
     global position
     position = position + 1 
@@ -129,12 +126,22 @@ def loadDisplay(Clk, value):
         GPIO.output(pin, state)
         GPIO.output(DP, GPIO.LOW)
         GPIO.output(Clk, GPIO.HIGH)
-        
-def startMode():
-    loadDisplay(Clk1, 'X')
-    loadDisplay(Clk2, 'X')
-    loadDisplay(Clk3, 'X')
-    loadDisplay(Clk4, 'X')
+
+def increment():
+    last[3] = int(last[3]) + 1
+    if int(last[3]) == 10:
+        last[2] = int(last [2]) + 1
+        last[3] = 0
+        if int(last [2]) == 6:
+            last [1] = int(last[1]) + 1
+            last [2] = 0
+            if int(last [0]) == 1 and int(last [1]) == 3:
+                last [0] = 0
+                last [1] = 1
+            elif int(last [1]) == 10:
+                last [0] = int(last [0]) + 1
+                last [1] = 0
+    loadLast()
 
 def autoClock():
     PM = False
@@ -158,19 +165,17 @@ def autoClock():
 
     # Returns the hour and the minute as 2 digit strings
     return hour, minute
-
-startMode()   
+   
 while True:
-    escape = 0
     GPIO.output([Clk1,Clk2,Clk3,Clk4], GPIO.LOW)
     value = read_keypad()
     print(value)
     if(value):
         print("reading")
-
+        
+        
         # This runs the automatic clock
         if value == 'A':
-            while True:
                 hourDigit, minuteDigit = autoClock()
                 # Separates the two digit strings into a single digit
                 hourDigit.split()
@@ -181,10 +186,7 @@ while True:
                 loadDisplay(Clk3, minuteDigit[0])
                 loadDisplay(Clk4, minuteDigit[1])
                 value = read_keypad()
-                if value == 'B':
-                    break
-        
-        elif value == 'B':
+        else:
             if position == 1:
                 loadDisplay(Clk1, value)
                 last[0] = value
@@ -207,6 +209,7 @@ while True:
         elif position == 4:
             flash(Clk4)
         else:
+            print("uh oh")
             now = time.time()
             switch = now + 60
             while(time.time() < switch):
@@ -223,16 +226,8 @@ while True:
                         loadLast()
                         count = count + 1
                         time.sleep(0.05)
-                        continue
-                
-                elif(value == 'B'):
-                    escape = escape + 1
-                    if(escape == 3):
-                        startMode()
-                        escape = 0
-                        position = 1
-                    break
-                    
+                        break
+                        
             last[3] = int(last[3]) + 1
             if int(last[3]) == 10:
                 last[2] = int(last [2]) + 1

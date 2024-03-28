6282038858196aa3774381526e5f866066d7434f
Evan Culbertson <esc93338@uga.edu>
2024-03-28 15:52:24 -0400

    Decoder stuff

--- Python/MorseCode/MCDecoder.py
+++ Python/MorseCode/MCDecoder.py
@@ -1,10 +1,13 @@
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
 
 # GPIO Setup
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
 # GPIO Setup
-GPIO.setup(18, GPIO.IN)
+GPIO.setup(23, GPIO.IN)
 GPIO.setup(25, GPIO.OUT)
 
 # Morse code mapping
@@ -27,51 +30,87 @@ MORSE_TO_LETTERS = {
 }
 
 
-global dot_length, morse, decoded, word
+global dot_length, morse, decoded, word, calibrated
 dot_length = 0
 morse = ""
 word = ""
 decoded = ""
+calibrated = False
 
-#attention
+# Attention
 def calibrate():
-    calibrated = False
+    global calibrated
     count = 0
     print ("Attention")
     print("tap: -.-.-")
     while(not calibrated):
-        if(GPIO.input(18)==1):
+        if(GPIO.input(23)==1):
+            time.sleep(0.1)
+            
+        
+            
+            
             if(count == 0):
-                dash_start = time.now()
+                dash_start = float(time.time())
                 count = count + 1
+                time.sleep(0.01)
+                while(GPIO.input(23) == 1):
+                    print("count = 1")
             elif(count == 1):
-                first_dash = time.now() - dash_start
+                first_dash = float(time.time()) - dash_start
                 count = count + 1
+                time.sleep(0.01)
+                while(GPIO.input(23) == 1):
+                    time.sleep(0.01)
             elif(count == 2):
-                dot_start = time.now()
+                dot_start = float(time.time())
                 count = count + 1
+                time.sleep(0.01)
+                while(GPIO.input(23) == 1):
+                    time.sleep(0.01)
             elif(count == 3):
-                first_dot = time.now()- dot_start
+                first_dot = float(time.time())- dot_start
                 count = count + 1
+                time.sleep(0.01)
+                while(GPIO.input(23) == 1):
+                    time.sleep(0.01)
             elif(count == 4):
-                dash_start = time.now()
+                dash_start = float(time.time())
                 count = count + 1
+                time.sleep(0.01)
+                while(GPIO.input(23) == 1):
+                    time.sleep(0.01)
             elif(count == 5):
-                second_dash = time.now() - dash_start
+                second_dash = float(time.time()) - dash_start
                 count = count + 1
+                time.sleep(0.01)
+                while(GPIO.input(23) == 1):
+                    time.sleep(0.01)
             elif(count == 6):
-                dot_start = time.now()
+                dot_start = float(time.time())
                 count = count + 1
+                time.sleep(0.01)
+                while(GPIO.input(23) == 1):
+                    time.sleep(0.01)
             elif(count == 7):
-                second_dot = time.now() - dot_start
+                second_dot = float(time.time()) - dot_start
                 count = count + 1
+                time.sleep(0.01)
+                while(GPIO.input(23) == 1):
+                    time.sleep(0.01)
             elif(count == 8):
-                dash_start = time.now()
+                dash_start = float(time.time())
                 count = count + 1
+                time.sleep(0.01)
+                while(GPIO.input(23) == 1):
+                    time.sleep(0.01)
             elif(count == 9):
-                third_dash = time.now()-dash_start
+                third_dash = float(time.time())-dash_start
+                time.sleep(0.01)
                 count = count + 1
                 calibrated = True
+                while(GPIO.input(23) == 1):
+                    time.sleep(0.01)
     
     dash_length = (first_dash + second_dash + third_dash)/3
     dot_length = (first_dot+second_dot)/2 
@@ -84,25 +123,27 @@ def calibrate():
 
 
     
-
-
+calibrate()
+print("calibrated")
 while(True):
-    if(GPIO.input(18) == 1):
-        start = time.now()
+    if(GPIO.input(23) == 1):
+        start = float(time.time())
         GPIO.output(25, 1)
-        while(GPIO.input(18) == 1):
-            print("wait")
+        while(GPIO.input(23) == 1):
+           time.sleep(0.01)
         GPIO.output(25, 0)
-        length = time.now() - start
+        length = float(time.time()) - start
         if(length<dot_length+dot_length*0.5):
             morse = morse +"."
+            print(morse)
         else:
             morse = morse + "-"
+            print(morse)
     elif(GPIO.input == 0):
-        start = time.now()
-        while(GPIO.input(18) == 0):
-            print("wait")
-        length = time.now() - start
+        start = float(time.time())
+        while(GPIO.input(23) == 0):
+            time.sleep(0.01)
+        length = float(time.time()) - start
         if(dot_length/2<length<dot_length+dot_length*0.5):
             #space between characters
             print("space between character")
@@ -113,6 +154,7 @@ while(True):
                 word = MORSE_TO_LETTERS[morse]
                 morse = ""
                 decoded = word + " "
+                print(decoded)
                 if (word == "out"):
                     break
                 else:

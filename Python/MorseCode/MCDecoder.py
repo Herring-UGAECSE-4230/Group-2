5f80a938be0c0d81a9a2c288fd69076fff6f46c4
Tyler Cook <twc35917@uga.edu>
2024-03-31 20:07:10 -0400

    "swapped to switch case because its better wnd reworked calibrate method still need to test"

--- Python/MorseCode/MCDecoder.py
+++ Python/MorseCode/MCDecoder.py
@@ -37,85 +37,63 @@ word = ""
 decoded = ""
 calibrated = False
 
-# Attention
+#Attention
 def calibrate():
     global calibrated
     count = 0
     print ("Attention")
     print("tap: -.-.-")
     while(not calibrated):
+        print("not in contact")
         if(GPIO.input(23)==1):
-            time.sleep(0.1)
+            time.sleep(0.05)
             
-        
-            
-            
-            if(count == 0):
-                dash_start = float(time.time())
-                count = count + 1
-                time.sleep(0.01)
-                while(GPIO.input(23) == 1):
-                    print("count = 1")
-            elif(count == 1):
-                first_dash = float(time.time()) - dash_start
-                count = count + 1
-                time.sleep(0.01)
-                while(GPIO.input(23) == 1):
+            #only increments the count after it is released
+           
+            match count:
+                case 0:
+                    dash_start = float(time.time())
                     time.sleep(0.01)
-            elif(count == 2):
-                dot_start = float(time.time())
-                count = count + 1
-                time.sleep(0.01)
-                while(GPIO.input(23) == 1):
+                case 1:
+                    first_dash = float(time.time()) - dash_start
                     time.sleep(0.01)
-            elif(count == 3):
-                first_dot = float(time.time())- dot_start
-                count = count + 1
-                time.sleep(0.01)
-                while(GPIO.input(23) == 1):
+                case 2:
+                    dot_start = float(time.time())
                     time.sleep(0.01)
-            elif(count == 4):
-                dash_start = float(time.time())
-                count = count + 1
-                time.sleep(0.01)
-                while(GPIO.input(23) == 1):
+                case 3:
+                    first_dot = float(time.time())- dot_start
                     time.sleep(0.01)
-            elif(count == 5):
-                second_dash = float(time.time()) - dash_start
-                count = count + 1
-                time.sleep(0.01)
-                while(GPIO.input(23) == 1):
+                case 4:
+                    dash_start = float(time.time())
                     time.sleep(0.01)
-            elif(count == 6):
-                dot_start = float(time.time())
-                count = count + 1
-                time.sleep(0.01)
-                while(GPIO.input(23) == 1):
+                case 5:
+                    second_dash = float(time.time()) - dash_start
                     time.sleep(0.01)
-            elif(count == 7):
-                second_dot = float(time.time()) - dot_start
-                count = count + 1
-                time.sleep(0.01)
-                while(GPIO.input(23) == 1):
+                case 6:
+                    dot_start = float(time.time())
                     time.sleep(0.01)
-            elif(count == 8):
-                dash_start = float(time.time())
-                count = count + 1
-                time.sleep(0.01)
-                while(GPIO.input(23) == 1):
+                case 7:
+                    second_dot = float(time.time()) - dot_start
                     time.sleep(0.01)
-            elif(count == 9):
-                third_dash = float(time.time())-dash_start
-                time.sleep(0.01)
-                count = count + 1
-                calibrated = True
-                while(GPIO.input(23) == 1):
+                case 8:
+                    dash_start = float(time.time())
                     time.sleep(0.01)
+                case 9:
+                    third_dash = float(time.time())-dash_start
+                    time.sleep(0.01)
+                    calibrated = True
+
+            #won't loop again until release  
+            while(GPIO.input(23)==1):
+                print("in contact")
+            #increment count
+            count = count + 1
+
     
     dash_length = (first_dash + second_dash + third_dash)/3
-    dot_length = (first_dot+second_dot)/2 
+    dot_length = (first_dot+second_dot)/2  + (dash_length/3)/2
     # trying to calibrate the most accurate dot using all data possible
-    dot_length = (dot_length + (dash_length/3)/2)
+    
 
 
             

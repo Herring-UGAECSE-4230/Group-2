02a0602a32dea01cc3a1d5d327058ee677e4ac73
Evan Culbertson <esc93338@uga.edu>
2024-04-04 15:44:09 -0400

    Decoder stuff

--- Python/MorseCode/MCDecoder.py
+++ Python/MorseCode/MCDecoder.py
@@ -39,7 +39,7 @@ calibrated = False
 
 #Attention
 def calibrate():
-    global calibrated
+    global calibrated,dot_length
     count = 0
     print ("Attention")
     print("tap: -.-.-")
@@ -52,49 +52,39 @@ def calibrate():
  
             if(count == 0):
                 dash_start = float(time.time())
-                time.sleep(0.01)
             elif(count == 1):
                 dot_start = float(time.time())
-                time.sleep(0.01)
             elif(count == 2):
                 dash_start = float(time.time())
-                time.sleep(0.01)
             elif(count == 3):
                 dot_start = float(time.time())
-                time.sleep(0.01)
             
             elif(count == 4):
                 dash_start = float(time.time())
-                time.sleep(0.01)
 
             #won't loop again until release  
             while(GPIO.input(23)==1):
                 print("in contact")
             #increment count
+            time.sleep(0.01)
             count = count + 1
         elif(GPIO.input(23) == 0):
-            time.sleep(0.05)
             if(count == 1):
                 first_dash = float(time.time()) - dash_start
-                time.sleep(0.01)
             elif(count == 2):
                 first_dot = float(time.time())- dot_start
-                time.sleep(0.01)
             elif(count == 3):
                 second_dash = float(time.time()) - dash_start
-                time.sleep(0.01)
             elif(count == 4):
                 second_dot = float(time.time()) - dot_start
-                time.sleep(0.01)
             elif (count ==  5):
                 third_dash = float(time.time())-dash_start
-                time.sleep(0.01)
                 calibrated = True
 
     
-    dash_length = (first_dash + second_dash + third_dash)/3
-    dot_length = (first_dot+second_dot)/2  + (dash_length/3)/2
+    dot_length = (first_dot+second_dot)/2  + ((first_dash + second_dash + third_dash)/9)/2
     print(dot_length)
+   
     # trying to calibrate the most accurate dot using all data possible
     
 
@@ -107,33 +97,51 @@ def calibrate():
 calibrate()
 print("calibrated")
 while(True):
-    if(GPIO.input(23) == 1):
+    if(GPIO.input(23) == 1): # If a signal is detected we go into "on" mode
         on = float(time.time())
-        
-        while(GPIO.input(23) == 1):
+        while(GPIO.input(23) == 1): # While the tapper is held down, a timer is running
            GPIO.output(25, 1)
            print("timing")
         GPIO.output(25, 0)
 
-        onLength = float(time.time()) - on
-        if(onLength<dot_length+dot_length*0.5):
+        onLength = float(time.time()) - on # Timer for "on" state of tapper
+        
+        if(onLength < dot_length*2): # If on length is less than 2 times unit length -> .
             morse = morse +"."
+            print(onLength)
             print(morse)
-        else:
+        else: # If on length is more than 2 times unit length -> -
             morse = morse + "-"
             print(morse)
-    elif(GPIO.input == 0):
+            
+    elif(GPIO.input(23) == 0): # If a signal is not detected we go into "off mode"
         off = float(time.time())
-        while(GPIO.input(23) == 0):
-            print("timing")
-        offLength = float(time.time()) - off
-        if(dot_length/2<offLength<dot_length+dot_length*0.5):
-            #space between characters
-            print("space between character")
-
-        elif(offLength>dot_length+dot_length*0.5):
-            #space between words
-            if(morse in MORSE_TO_LETTERS ):
+        while(GPIO.input(23) == 0): 
+            GPIO.output(25, 0)
+        
+        offLength = float(time.time()) - off # Timer for "off" state of tapper
+        
+        # Conditionals to determine what is displaying
+        if(offLength < dot_length*2): # If off time is less than 2 times unit length -> Morse code
+            print("Morse Code")
+            time.sleep(0.01)
+            
+        elif(dot_length*5 > offLength > dot_length*2): # If off time is between 2 times unit length and 5 times unit length -> Space between character
+            # Space between characters
+            print("Space between CHARACTER")
+            time.sleep(0.01)
+            
+            if(morse in MORSE_TO_LETTERS):
+                character = MORSE_TO_LETTERS[morse]
+                word = word + character
+                morse = ""
+                print(word)
+                
+        elif(offLength > dot_length*5): # If off time is greater than 5 times the unit length -> Space between word
+            # Space between words
+            print("Space between WORD")
+            time.sleep(0.01)
+            if(morse in MORSE_TO_LETTERS):
                 word = MORSE_TO_LETTERS[morse]
                 morse = ""
                 decoded = word + " "
